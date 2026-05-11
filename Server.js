4eb82fbf4f0b9f9d2d5b77fa67809009875edcff
Suryabharani <suryahari2004s@gmail.com>
2026-05-11 09:31:35 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,1 +1,19 @@
 
+// server.js
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let campaigns = [];
+
+app.get('/campaigns', (req, res) => {
+  res.json(campaigns);
+});
+
+app.post('/campaigns', (req, res) => {
+  campaigns.push(req.body);
+  res.send("Added");
+});
+
+app.listen(5000);
